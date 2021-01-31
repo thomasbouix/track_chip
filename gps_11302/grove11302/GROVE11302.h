4d44888177a00179a7e9a04e4d7acda2476552a0
Enzo <enzocalvino3@gmail.com>
2021-01-31 12:31:22 +0100

    update header

--- gps_11302/grove11302/GROVE11302.h
+++ gps_11302/grove11302/GROVE11302.h
@@ -13,6 +13,7 @@ public:
 	unsigned char buffer[70];	// NMEA data, Example:  ,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47  
 
 	GROVE11302();
+	GROVE11302(int baudrate, int RX, int TX);
 	virtual ~GROVE11302();
 
 	// Fill buffer attribute with NMEA data
