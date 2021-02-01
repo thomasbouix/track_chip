5abd76014696387dae50b2a7951d3c06c8232c69
thomasbouix <thomas.bouix@etu.sorbonne-universite.fr>
2021-02-01 13:15:54 +0100

    refactoring

--- devices/system/system.ino
+++ devices/system/system.ino
@@ -9,30 +9,30 @@ GROVE11302 gps_grove = GROVE11302();
 
 float altitude;
 
+void print_data() {
+
+	altitude = bmp180.computeAltitude();
+	gps_grove.get_data_line();
+
+	Serial.print("\n=============\n");
+	Serial.print("buffer : "); 
+	Serial.write(gps_grove.buffer, 70);
+	Serial.print("altitude : ");
+	Serial.print(altitude);
+	Serial.println("m");	
+	Serial.print("\n=============\n");
+}
+
 void setup() {
 
 	Serial.begin(115200); 
 	while (!Serial);
-
 	Wire.begin();
-
 	bmp180.init();
-
 }
 
 void loop() {
 
-	altitude = bmp180.computeAltitude();
-  gps_grove.get_data_line();
-
-  Serial.print("\n=============\n");
-  Serial.print("buffer : "); 
-  Serial.write(gps_grove.buffer, 70);
-	Serial.print("altitude : ");
-	Serial.print(altitude);
-	Serial.println("m");	
-  Serial.print("\n=============\n");
-
+	print_data();
 	delay(1000);
-
 }
