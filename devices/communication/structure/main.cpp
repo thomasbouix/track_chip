6d7ccd74f54fb059aa9c8bbfe775405d5efb7a80
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-04 12:48:17 +0100

    Dms_to_dd

--- devices/communication/structure/main.cpp
+++ devices/communication/structure/main.cpp
@@ -2,11 +2,32 @@
 
 #include <iostream>
 #include "Structure_Sigfox.h"
+
+// convertis aussi bien la latitude que la longitude. Cette fonction suppose que le paramètre cardinal est N, S, E ou O.
+double Dms_to_dd(char cardinal, int angle, int minute, double seconde){
+	double res = angle;
+	double temp = seconde/60;
+	res += (minute+temp)/60;
+	if( cardinal == 'S' || cardinal == 'O') { res = -res;}
+	return res;ha
+	}
+
 int main() {
-    std::cout << "Hello World!";
     Message m = Message();
     string res;
-    res = m.float_to_hexa(3.14);
-    std::cout << res;
-    return 0;
+    res = m.int_to_hexa(314);
+    //std::cout << res;
+    double lat =  48.84527;
+    int prec = 5;
+    int res_int;
+    res_int= m.double_to_int(lat, prec);
+    //std::cout << res_int<<endl;
+    
+    res = m.double_to_hexa(lat,prec);//tester sur un plage de valeur d'etrée de 0 à 270;
+    //std::cout << res;
+    
+    
+    std::cout<<Dms_to_dd('S',48, 51, 17.005)<<endl;
+    std::cout<<Dms_to_dd('O',2, 21, 0.749)<<endl;
+    
 }
