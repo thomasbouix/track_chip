8fd64481eca1cfd0def5d476a2b50801a2403333
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-04 15:12:45 +0100

    complete_hexa_bytes

--- devices/communication/structure/main.cpp
+++ devices/communication/structure/main.cpp
@@ -11,6 +11,23 @@ double Dms_to_dd(char cardinal, int angle, int minute, double seconde){
 	if( cardinal == 'S' || cardinal == 'O') { res = -res;}
 	return res;
 	}
+double shift_latitude(double lat){
+	return lat+180;
+}
+
+double shift_longitude(double lng){
+	return lng+90;
+}
+
+string complete_hexa_bytes(string hexa, int nb_bytes){ // prend en argument une chaine hexa et le nombre d'octets voulu pour le callback et complete avec des 0 big edians
+	int add = 2*nb_bytes - hexa.length();
+	string complete = "";
+	for(int i = 0; i<add;i++){
+	complete = complete+"0";
+	}
+	return complete +hexa;
+}
+
 
 int main() {
     Message m = Message();
@@ -29,8 +46,8 @@ int main() {
     //std::cout<<Dms_to_dd('N',48, 51, 17.005)<<endl;
     //std::cout<<Dms_to_dd('E',2, 21, 0.749)<<endl; // !!!Attention!!! Ne tient pas compte du signe ==> int_to_hexa
     std::cout<<"Test Général"<<endl;
-    lat = Dms_to_dd('N',48, 51, 17.005);
-    double lng = Dms_to_dd('E',2, 21, 0.749);
+    lat = shift_latitude(Dms_to_dd('N',48, 51, 17.005));
+    double lng = shift_longitude(Dms_to_dd('E',2, 21, 0.749));
     std::cout << lat<<endl;
     std::cout << lng<<endl;
     
@@ -38,9 +55,14 @@ int main() {
     
     string res_lat = m.double_to_hexa(lat,prec);
     std::cout << res_lat<<endl;
+    std::cout <<complete_hexa_bytes(res_lat,4)<<endl;
     string res_lng = m.double_to_hexa(lng,prec);
-     std::cout << res_lng<<endl;
-    res = res_lat+res_lng+m.int_to_hexa(altitude);
+    std::cout << res_lng<<endl;
+    std::cout <<complete_hexa_bytes(res_lng,4)<<endl;
+    string res_alt = m.int_to_hexa(altitude);
+    std::cout << res_alt<<endl;
+    std::cout <<complete_hexa_bytes(res_alt,2)<<endl;
+    res = res_lat+res_lng+ res_alt;
     std::cout << res<<endl;
     
     
