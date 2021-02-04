3b9118f51b0439abfe53501e4ea27e298ab593bb
Enzo <enzocalvino3@gmail.com>
2021-02-04 11:30:58 +0100

    update-https_client

--- devices/wifi_esp32/examples/https_post/https_post.ino
+++ devices/wifi_esp32/examples/https_post/https_post.ino
@@ -1,71 +1,80 @@
 #include <ArduinoJson.h>
 #include <HTTPClient.h>
-#include "WiFiMulti.h"
- 
-const char *AP_SSID = "Redmi";
-const char *AP_PWD = "toto1234";
-  
-WiFiMulti wifiMulti;
+#include <WiFi.h>
+
+// Eugeune's mobile network
+const char *AP_SSID = "Minn";
+const char *AP_PWD = "arun5505";
+
+const char  *serverAddress  = "https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDMnY8W47H_ztdC4sJjo2Z9_bu2y9-zEPM";
+int          port           = 8080;
  
 void setup() {
   Serial.begin(115200);
-   
-  delay(4000);
-  wifiMulti.addAP(AP_SSID, AP_PWD);
- 
-  postDataToServer();
+    
+  WiFi.begin(AP_SSID, AP_PWD);
+  
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println("Setup done");
 }
  
 void loop() {
-  // Not used in this example
-}
- 
-void postDataToServer() {
- 
   Serial.println("Posting JSON data to server...");
 
   // Block until we are able to connect to the WiFi access point
-  if (wifiMulti.run() == WL_CONNECTED) {
-     
+  if (WiFi.status() == WL_CONNECTED) {
+
     HTTPClient http;   
      
-    http.begin("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDMnY8W47H_ztdC4sJjo2Z9_bu2y9-zEPM");  
-    http.addHeader("Content-Type", "application/json");         
+    http.begin(serverAddress);  
+    http.addHeader("Content-Type", "application/json");           
      
-    StaticJsonDocument<256> doc;
+    StaticJsonDocument<256> req;
 
-    doc["considerIp"] = "false";
+    JsonArray wifiAccessPoints = req.createNestedArray("wifiAccessPoints");
 
-    JsonArray wifiAccessPoints = doc.createNestedArray("wifiAccessPoints");
-
-    JsonObject wifiAccessPoints_0 = wifiAccessPoints.createNestedObject();
-    wifiAccessPoints_0["macAddress"] = "70:62:B8:51:26:20";
-    wifiAccessPoints_0["signalStrength"] = -57;
-    wifiAccessPoints_0["signalToNoiseRatio"] = 0;
-
-    JsonObject wifiAccessPoints_1 = wifiAccessPoints.createNestedObject();
-    wifiAccessPoints_1["macAddress"] = "EA:A6:98:D0:A1:BA";
-    wifiAccessPoints_1["signalStrength"] = -58;
-    wifiAccessPoints_1["signalToNoiseRatio"] = 0;
+    // Warning : mac Adress and Signal Strenght have to be updated before using this code
+    JsonObject wifiAccessPoints_0           = wifiAccessPoints.createNestedObject();
+    wifiAccessPoints_0["macAddress"]        = "80-EA-9D-90-CD-5A";
+    wifiAccessPoints_0["signalStrength"]    = -59;
+    
+    JsonObject wifiAccessPoints_1           = wifiAccessPoints.createNestedObject();
+    wifiAccessPoints_1["macAddress"]        = "0-29-74-C5-6F-E6";
+    wifiAccessPoints_1["signalStrength"]    = -63;
+    
+    JsonObject wifiAccessPoints_2           = wifiAccessPoints.createNestedObject();
+    wifiAccessPoints_2["macAddress"]        = "20-26-51-B8-62-70";
+    wifiAccessPoints_2["signalStrength"]    = -65;
      
     String requestBody;
-    serializeJson(doc, requestBody);
+    serializeJson(req, requestBody);
+
+    Serial.println("");
+    Serial.print(requestBody);
+    Serial.println("");
      
     int httpResponseCode = http.POST(requestBody);
- 
+
     if(httpResponseCode>0){
        
-      String response = http.getString();                       
-       
+      String response = http.getString(); 
+
       Serial.println(httpResponseCode);   
       Serial.println(response);
-     
     }
     else {
-     
-      Serial.printf("Error occurred while sending HTTP POST: %s\n", httpClient.errorToString(statusCode).c_str());
-       
+      Serial.printf("Error occurred while sending HTTP POST");
     }
-     
+
+    while(1){}
   }
 }
