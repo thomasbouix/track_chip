343c918047ba53df203e6abfbaecd6f4fd2469aa
Enzo <enzocalvino3@gmail.com>
2021-02-04 10:43:51 +0100

    update-wifi_scan

--- devices/wifi_esp32/examples/wifi_scan/wifi_scan.ino
+++ devices/wifi_esp32/examples/wifi_scan/wifi_scan.ino
@@ -1,4 +1,5 @@
-#include "WiFi.h"
+#include <SPI.h>
+#include <WiFi.h>
 
 void setup()
 {
@@ -15,7 +16,10 @@ void setup()
 void loop()
 {
     Serial.println("scan start");
-    
+        
+    uint8_t *bssid;
+    char bssid_str[18];
+
     // WiFi.scanNetworks will return the number of networks found
     int n = WiFi.scanNetworks();
     Serial.println("scan done");
@@ -28,7 +32,10 @@ void loop()
         
         for (int i = 0; i < n; ++i) {
             
-            // Print SSID and RSSI for each network found
+            bssid = WiFi.BSSID(i);
+            sprintf(bssid_str, "%X-%X-%X-%X-%X-%X",bssid[5], bssid[4], bssid[3], bssid[2], bssid[1], bssid[0]); 
+
+            // Print SSID, RSSI, BSSID and encryption mode for each network found
             
             Serial.print(i + 1);
             Serial.print(": ");
@@ -38,22 +45,41 @@ void loop()
             Serial.print(", Power (dBm) :");
             Serial.print(WiFi.RSSI(i));
             
-            Serial.print(", MAC : ");
-            Serial.print(WiFi.BSSIDstr(i));
-            
-            Serial.print(", chan : ");
-            Serial.print(WiFi.channel(i));
+            Serial.print(", BSSID: ");
+            Serial.print(bssid_str);
 
-            Serial.print(", authmode : ");
-            Serial.print(WiFi.encryptionType(i));
+            Serial.print(", Encryption Type : ");
+            printEncryptionType(WiFi.encryptionType(i));
             
-            Serial.print(", ");
-            Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
             delay(10);
         }
     }
     Serial.println("");
 
-    // Wait a bit before scanning again
-    delay(20000);
+    // Wait for 5s before scanning again
+    delay(5000);
+}
+
+void printEncryptionType(int thisType) {
+  // read the encryption type and print out the name:
+  switch (thisType) {
+    case 5:
+      Serial.println("WEP");
+      break;
+    case 2:
+      Serial.println("WPA");
+      break;
+    case 4:
+      Serial.println("WPA2");
+      break;
+    case 7:
+      Serial.println("None");
+      break;
+    case 8:
+      Serial.println("Auto");
+      break;
+    default:
+      Serial.println("Unknown");
+      break;
+  }
 }
