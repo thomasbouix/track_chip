4e277c0eda84e3ad6a7beb3aa5b9dc23e25e4515
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-04 13:22:12 +0100

    Test assemblage des fonctions : int_to_hexa bug pour nombre négatif

--- devices/communication/structure/main.cpp
+++ devices/communication/structure/main.cpp
@@ -9,13 +9,14 @@ double Dms_to_dd(char cardinal, int angle, int minute, double seconde){
 	double temp = seconde/60;
 	res += (minute+temp)/60;
 	if( cardinal == 'S' || cardinal == 'O') { res = -res;}
-	return res;ha
+	return res;
 	}
 
 int main() {
     Message m = Message();
+    
     string res;
-    res = m.int_to_hexa(314);
+    //res = m.int_to_hexa(314);
     //std::cout << res;
     double lat =  48.84527;
     int prec = 5;
@@ -24,10 +25,23 @@ int main() {
     //std::cout << res_int<<endl;
     
     res = m.double_to_hexa(lat,prec);//tester sur un plage de valeur d'etrée de 0 à 270;
-    //std::cout << res;
+    //std::cout << res<<endl;
+    //std::cout<<Dms_to_dd('N',48, 51, 17.005)<<endl;
+    //std::cout<<Dms_to_dd('E',2, 21, 0.749)<<endl; // !!!Attention!!! Ne tient pas compte du signe ==> int_to_hexa
+    std::cout<<"Test Général"<<endl;
+    lat = Dms_to_dd('N',48, 51, 17.005);
+    double lng = Dms_to_dd('E',2, 21, 0.749);
+    std::cout << lat<<endl;
+    std::cout << lng<<endl;
     
+    int altitude = 55;
     
-    std::cout<<Dms_to_dd('S',48, 51, 17.005)<<endl;
-    std::cout<<Dms_to_dd('O',2, 21, 0.749)<<endl;
+    string res_lat = m.double_to_hexa(lat,prec);
+    std::cout << res_lat<<endl;
+    string res_lng = m.double_to_hexa(lng,prec);
+     std::cout << res_lng<<endl;
+    res = res_lat+res_lng+m.int_to_hexa(altitude);
+    std::cout << res<<endl;
+    
     
 }
