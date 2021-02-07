1330b3175a0ba58ab40f14a420123d806f690abb
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-07 12:09:32 +0100

    suppression double setup/loop

--- devices/communication/sigfox/examples/wisol_conversion/wisol_conversion.ino
+++ devices/communication/sigfox/examples/wisol_conversion/wisol_conversion.ino
@@ -34,12 +34,4 @@ void loop() {
     Serial.println(retour);
   }
 
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
