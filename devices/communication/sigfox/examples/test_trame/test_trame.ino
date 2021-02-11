d02b17746208054708cfac77ed6023c5ebe4b004
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-11 12:13:14 +0100

    Refactoring : déplacement de l'exemple

--- devices/communication/sigfox/examples/test_trame/test_trame.ino
+++ devices/communication/sigfox/examples/test_trame/test_trame.ino
@@ -1,10 +1,6 @@
 #include "Wisol.h"
 void setup() {
-  // put your setup code here, to run once:
-  //Wisol w;
   Serial.begin(115200);
-  //Serial.println(w.verif_type("$GPGGA,064036.289,4836.5375,N,00740.9373,E,1,04,3.2,200.2,M,,,,0000*0E"));
-  
 }
 
 void loop() {
