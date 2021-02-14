e1d6ad95f5f78da121f94f964894d5c40f7fe47e
Enzo <enzocalvino3@gmail.com>
2021-02-14 15:01:16 +0100

    dev-login_php-add_redirections

--- cloud/php/login.php
+++ cloud/php/login.php
@@ -1,8 +1,5 @@
 <?php
-
   // Recupere les donnees postee par Sigfox et les insere dans la base de donnee
-
-  // echo "On rentre dans login.php\n";
 
   // Receive Data from SigFox
   if (isset($_POST['username'], $_POST['password'])) 
@@ -18,6 +15,7 @@
   // Connection to database
   try
   {
+    // system('sqlite3 ../database/track_chip.db -cmd ".read ../database/track_chip.sql" ".quit"');
     $db = new SQLite3('../database/track_chip.db');
   }
   catch(Exception $e)
@@ -25,21 +23,9 @@
     die('Erreur : '.$e->getMessage());
   }
 
-  // echo "username : ";
-  // echo $username;
-  // echo "\n";
-
-  // echo "password : ";
-  // echo $password;
-  // echo "\n";
-
   // Insert in to database
   $response = $db->query('SELECT * FROM Owner WHERE username="' .$username. '" AND password="' .$password. '";');
   $resutl = $response->fetchArray(1);
-
-  // echo "result : ";
-  // print_r($resutl);
-  // echo "\n";
 
   if (empty($resutl) == false) 
   {
@@ -49,8 +35,8 @@
   }
   else
   {
-    // header('Location: https://track-chip.alwaysdata.net/index.php');
-    header('Location: http://127.0.0.1:8888/index.php');
+    // header('Location: https://track-chip.alwaysdata.net/index.php?auth_failed=1');
+    header('Location: http://127.0.0.1:8888/index.php?auth_failed=1');
     exit();
   }
 
