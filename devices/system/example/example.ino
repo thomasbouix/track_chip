fea8d640928a7d616a817b85b648533898906bb7
EugeneRaphanel <raphanel.eugene@gmail.com>
2021-02-17 15:30:04 +0100

    Test part_bssid_to_hexa

--- devices/system/example/example.ino
+++ devices/system/example/example.ino
@@ -39,15 +39,21 @@ void loop() {
   Serial.println("========");
   Serial.print("altitude : "); Serial.print(altitude); Serial.println("m");
   Serial.print("position : "); Serial.println(position);
-  Serial.print("res : ");
-  /*String res = trackChip.create_message1();
+  /*Serial.print("res : ");
+  String res = trackChip.create_message1();
   Serial.println(res);
+  Serial.print("Taille message 1 : ");
+  Serial.println(res.length());
   res = trackChip.create_message2();
   Serial.println(res);
+  Serial.print("Taille message 2 : ");
+  Serial.println(res.length());
   res = trackChip.create_message3();
-  Serial.println(res);*/
+  Serial.println(res);
+  Serial.print("Taille message 3 : ");
+  Serial.println(res.length());*/
   trackChip.chose_message_to_send();
-  
+
   while(1);
 
 }
